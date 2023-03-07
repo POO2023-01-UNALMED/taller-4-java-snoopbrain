27feac9f24bb3f8363ae107909edfc65c0f99fbc
snoopbrain <ancarvajalb@unal.edu.co>
2023-03-07 11:16:54 -0500

    sdasd

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -3,7 +3,7 @@ package classroom;
 public class Persona {
     
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
@@ -12,46 +12,38 @@ public class Persona {
         //cedula = 3;
     }
 
-    Persona(long cedula, String nombre) {
+    public Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
         totalPersonas++;
     }
     
-    Persona(String nombre, long cedula) {
+    public Persona(String nombre, long cedula) {
         this.cedula = cedula;
         this.nombre = nombre;
         totalPersonas++;
     }
 
-    Persona(long cedula) {
+    public Persona(long cedula) {
         this.cedula = cedula;
         this.nombre = "";
         totalPersonas++;
     }
 
-    Persona(String nombre) {
-        this.cedula = 1;
+    public Persona(String nombre) {
         this.nombre = nombre;
+        this.cedula = 1;
         totalPersonas++;
     }
     
-    // Persona(){
-    //     this.nombre = "";
-    //     this.cedula = 0;
-    //     totalPersonas++;
-    // }
-    Persona(){
-        this.cedula = 0;
-    }
-    
-    long getCedula() {
+    public long getCedula(){
         return cedula;
     }
-    String getNombre(){
+    
+    public String getNombre(){
         return nombre;
     }
-    void setNombre(String nombre){
+    public void setNombre(String nombre){
         this.nombre=nombre;
     }
 }
